d0ae927d3e06043af2e5943d61099e5a338360ba
ComputadorasySensores <79330985+ComputadorasySensores@users.noreply.github.com>
2021-02-19 15:30:17 -0300

    Create bme280-ver1.py
    Script inicial

---
+++ bme280-ver1.py
@@ -0,0 +1,15 @@
+try:
+    from smbus2 import SMBus
+except ImportError:
+    from smbus import SMBus
+from bme280 import BME280
+
+# Inicializa el BME280
+bus = SMBus(1)
+bme280 = BME280(i2c_dev=bus)
+
+while True:
+    temperatura = bme280.get_temperature()
+    presion = bme280.get_pressure()
+    humedad = bme280.get_humidity()
+    print('{:05.2f}*C {:05.2f}hPa {:05.2f}%'.format(temperatura, presion, humedad))
